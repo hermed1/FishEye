633bd330379c5c18b2dfbf969302fa37163a281e
Mehdi Charef <me-charef@hotmail.fr>
2024-02-10 17:25:54 +0100

    no desc sorting + changed dropdown logic

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -8,10 +8,93 @@ const id = params.get('id');
 
 let displayedMedias;
 let photographer;
-let isSortedByPopularityAsc = true;
-let isSortedByDateAsc = true;
-let isSortedByTitleAsc = true;
+// let isSortedByPopularityAsc = true;
+// let isSortedByDateAsc = true;
+// let isSortedByTitleAsc = true;
 let isDropdownOpen = false;
+
+const buttonsList = [
+  { id: 1, label: 'Popularité', sortingFunction: sortByPopularity },
+  { id: 2, label: 'Titre', sortingFunction: sortByTitle },
+  { id: 3, label: 'Date', sortingFunction: sortByDate },
+];
+
+// function generateDropdownButtons() {
+//   const dropdownButtons = document.querySelector('.dropdown-buttons');
+
+//   if (!isDropdownOpen) {
+//     const element = buttonsList[0];
+//     const button = document.createElement('button');
+//     button.textContent = element.label;
+//     button.classList.add('dropdown-button');
+//     dropdownButtons.appendChild(button);
+//   }
+//   for (let element of buttonsList) {
+//     const button = document.createElement('button');
+//     button.textContent = element.label;
+//     button.classList.add('dropdown-button');
+//     dropdownButtons.appendChild(button);
+//     button.addEventListener('click', () => {
+//       element.sortingFunction();
+//     });
+//   }
+// }
+
+function generateDropdownButtons() {
+  const dropdownButtons = document.querySelector('.dropdown-buttons');
+  // Vider le conteneur avant d'ajouter de nouveaux boutons
+  dropdownButtons.innerHTML = '';
+
+  if (!isDropdownOpen) {
+    // Affiche uniquement le premier bouton si le menu est fermé
+    const element = buttonsList[0];
+    const button = document.createElement('button');
+    button.textContent = element.label;
+    button.classList.add('dropdown-button');
+    dropdownButtons.appendChild(button);
+    button.addEventListener('click', (event) => {
+      event.stopPropagation(); // Empêche l'événement de clic de se propager
+      // Ouvre le menu et regénère les boutons
+      isDropdownOpen = true;
+      generateDropdownButtons();
+      toggleDropdownArrows();
+    });
+  } else {
+    // Affiche tous les boutons si le menu est ouvert
+
+    for (let element of buttonsList) {
+      const dropdownButton = document.createElement('button');
+      dropdownButton.textContent = element.label;
+      dropdownButton.classList.add('dropdown-button');
+      dropdownButtons.appendChild(dropdownButton);
+      dropdownButton.addEventListener('click', () => {
+        element.sortingFunction();
+        // Déplace l'élément cliqué en première position du tableau
+        //avec unshift j'insère l'élément cliqué à [0]
+        buttonsList.unshift(
+          //mais je veux supprimer l'élément cliqué pour pas qu'il soit en double = splice(index de début, 1 élément à supprimer)
+          //je récupère l'élément supprimé avec [0] pour pouvoir l'insérer avec unshift
+          buttonsList.splice(buttonsList.indexOf(element), 1)[0]
+        );
+        isDropdownOpen = false; // Ferme le menu après sélection
+        generateDropdownButtons();
+        toggleDropdownArrows();
+      });
+    }
+    toggleDropdownArrows();
+  }
+}
+
+document.addEventListener('click', (event) => {
+  const dropdown = document.querySelector('.dropdown');
+  const isDropdownClicked = dropdown.contains(event.target);
+  if (!isDropdownClicked) {
+    generateDropdownButtons();
+    toggleDropdownArrows();
+    console.log('test');
+    isDropdownOpen = false;
+  }
+});
 
 async function createMedias(photographer) {
   let mediasContainer = document.querySelector('.medias-container');
@@ -74,168 +157,97 @@ async function initPhotographerPage() {
     const body = document.getElementsByTagName('body');
     body[0].appendChild(priceTag);
   }
+  generateDropdownButtons();
 }
 
-// function displayArrows() {
-//   if (isSortedByPopularityAsc) {
-//     const buttonPopularity = document.querySelector('.option1');
-//     const arrowDown = buttonPopularity.querySelector('.fa-chevron-down');
-//     const arrowUp = buttonPopularity.querySelector('.fa-chevron-up');
-//     arrowDown.style.display = 'none';
-//     arrowUp.style.display = 'inline';
-//   } else {
-//     const buttonPopularity = document.querySelector('.option1');
-//     const arrowDown = buttonPopularity.querySelector('.fa-chevron-down');
-//     const arrowUp = buttonPopularity.querySelector('.fa-chevron-up');
-//     arrowUp.style.display = 'none';
-//     arrowDown.style.display = 'inline';
-//   }
-//   if (isSortedByDateAsc) {
-//     const buttonDate = document.querySelector('.option2');
-//     const arrowDown = buttonDate.querySelector('.fa-chevron-down');
-//     const arrowUp = buttonDate.querySelector('.fa-chevron-up');
-//     arrowDown.style.display = 'none';
-//     arrowUp.style.display = 'inline';
-//   } else {
-//     const buttonDate = document.querySelector('.option2');
-//     const arrowDown = buttonDate.querySelector('.fa-chevron-down');
-//     const arrowUp = buttonDate.querySelector('.fa-chevron-up');
-//     arrowUp.style.display = 'none';
-//     arrowDown.style.display = 'inline';
-//   }
-//   if (isSortedByTitleAsc) {
-//     const buttonTitle = document.querySelector('.option3');
-//     const arrowDown = buttonTitle.querySelector('.fa-chevron-down');
-//     const arrowUp = buttonTitle.querySelector('.fa-chevron-up');
-//     arrowDown.style.display = 'none';
-//     arrowUp.style.display = 'inline';
-//   } else {
-//     const buttonTitle = document.querySelector('.option3');
-//     const arrowDown = buttonTitle.querySelector('.fa-chevron-down');
-//     const arrowUp = buttonTitle.querySelector('.fa-chevron-up');
-//     arrowUp.style.display = 'none';
-//     arrowDown.style.display = 'inline';
-//   }
+function sortByPopularity() {
+  displayedMedias.sort((a, b) => b.likes - a.likes);
+  console.log('Tri par popularité', displayedMedias);
+  createMedias(photographer);
+}
+
+function sortByDate() {
+  displayedMedias.sort((a, b) => new Date(a.date) - new Date(b.date));
+  console.log('Tri par date', displayedMedias);
+  createMedias(photographer);
+}
+
+function sortByTitle() {
+  displayedMedias.sort((a, b) => a.title.localeCompare(b.title));
+  console.log('Tri par titre', displayedMedias);
+  createMedias(photographer);
+}
+
+// function toggleOptions() {
+//   // Sélectionne les éléments HTML qui représentent les options de tri.
+//   const option1 = document.querySelector('.option1');
+//   const option2 = document.querySelector('.option2');
+//   const option3 = document.querySelector('.option3');
+
+//   // Ajoute un écouteur d'événement 'click' sur option1.
+//   option1.addEventListener('click', () => {
+//     if (!isDropdownOpen) {
+//       option2.classList.remove('not-displayed-options');
+//       option3.classList.remove('not-displayed-options');
+//       toggleDropdownArrows();
+//       isDropdownOpen = true;
+//     } else {
+//       // Lorsque option1 est cliquée, appelle toggleSelectedOption avec option1 comme l'option sélectionnée.
+//       toggleSelectedOption(option1, option2, option3);
+//     }
+//   });
+
+//   // Ajoute un écouteur d'événement 'click' sur option2, similaire à option1.
+//   option2.addEventListener('click', () => {
+//     toggleSelectedOption(option2, option1, option3);
+//   });
+
+//   // Ajoute un écouteur d'événement 'click' sur option3, similaire à option1 et option2.
+//   option3.addEventListener('click', () => {
+//     toggleSelectedOption(option3, option1, option2);
+//   });
 // }
 
-function toggleSortPopularity() {
-  displayedMedias.sort((a, b) =>
-    isSortedByPopularityAsc ? a.likes - b.likes : b.likes - a.likes
-  );
-  console.log('Tri par popularité', displayedMedias);
-  isSortedByPopularityAsc = !isSortedByPopularityAsc;
-  createMedias(photographer);
-}
+// // Définit la fonction qui gère le changement d'option sélectionnée.
+// function toggleSelectedOption(selectedOption, otherOption1, otherOption2) {
+//   // Vérifie le contenu textuel de l'option sélectionnée pour déterminer quelle fonction de tri appeler.
+//   if (selectedOption.textContent.includes('Popularité')) {
+//     // Si le texte inclut 'Popularité', appelle la fonction de tri par popularité.
+//     sortByPopularity();
+//   } else if (selectedOption.textContent.includes('Date')) {
+//     // Si le texte inclut 'Date', appelle la fonction de tri par date.
+//     sortByDate();
+//   } else if (selectedOption.textContent.includes('Titre')) {
+//     // Si le texte inclut 'Titre', appelle la fonction de tri par titre.
+//     sortByTitle();
+//   }
 
-function toggleSortDate() {
-  displayedMedias.sort((a, b) =>
-    isSortedByDateAsc
-      ? new Date(a.date) - new Date(b.date)
-      : new Date(b.date) - new Date(a.date)
-  );
-  console.log('Tri par date', displayedMedias);
-  isSortedByDateAsc = !isSortedByDateAsc;
-  createMedias(photographer);
-}
+//   // Affiche toutes les options en retirant la classe 'not-displayed-options' qui pourrait les cacher.
+//   selectedOption.classList.remove('not-displayed-options');
+//   otherOption1.classList.remove('not-displayed-options');
+//   otherOption2.classList.remove('not-displayed-options');
 
-function toggleSortTitle() {
-  displayedMedias.sort((a, b) =>
-    isSortedByTitleAsc
-      ? a.title.localeCompare(b.title)
-      : b.title.localeCompare(a.title)
-  );
-  console.log('Tri par titre', displayedMedias);
-  isSortedByTitleAsc = !isSortedByTitleAsc;
-  createMedias(photographer);
-}
+//   // Réorganise les options en déplaçant l'option sélectionnée en première position dans le conteneur '.dropdown-buttons'.
+//   const dropdown = document.querySelector('.dropdown-buttons');
+//   dropdown.insertBefore(selectedOption, dropdown.firstChild);
+// }
 
-function toggleOptions() {
-  // Sélectionne les éléments HTML qui représentent les options de tri.
-  const option1 = document.querySelector('.option1');
-  const option2 = document.querySelector('.option2');
-  const option3 = document.querySelector('.option3');
-
-  // Ajoute un écouteur d'événement 'click' sur option1.
-  option1.addEventListener('click', () => {
-    if (!isDropdownOpen) {
-      option2.classList.remove('not-displayed-options');
-      option3.classList.remove('not-displayed-options');
-      toggleDropdownArrows();
-      isDropdownOpen = true;
-    } else {
-      // Lorsque option1 est cliquée, appelle toggleSelectedOption avec option1 comme l'option sélectionnée.
-      toggleSelectedOption(option1, option2, option3);
-    }
-  });
-
-  // Ajoute un écouteur d'événement 'click' sur option2, similaire à option1.
-  option2.addEventListener('click', () => {
-    toggleSelectedOption(option2, option1, option3);
-  });
-
-  // Ajoute un écouteur d'événement 'click' sur option3, similaire à option1 et option2.
-  option3.addEventListener('click', () => {
-    toggleSelectedOption(option3, option1, option2);
-  });
-}
-
-// Définit la fonction qui gère le changement d'option sélectionnée.
-function toggleSelectedOption(selectedOption, otherOption1, otherOption2) {
-  // Vérifie le contenu textuel de l'option sélectionnée pour déterminer quelle fonction de tri appeler.
-  if (selectedOption.textContent.includes('Popularité')) {
-    // Si le texte inclut 'Popularité', appelle la fonction de tri par popularité.
-    toggleSortPopularity();
-  } else if (selectedOption.textContent.includes('Date')) {
-    // Si le texte inclut 'Date', appelle la fonction de tri par date.
-    toggleSortDate();
-  } else if (selectedOption.textContent.includes('Titre')) {
-    // Si le texte inclut 'Titre', appelle la fonction de tri par titre.
-    toggleSortTitle();
-  }
-
-  // Affiche toutes les options en retirant la classe 'not-displayed-options' qui pourrait les cacher.
-  selectedOption.classList.remove('not-displayed-options');
-  otherOption1.classList.remove('not-displayed-options');
-  otherOption2.classList.remove('not-displayed-options');
-
-  // Réorganise les options en déplaçant l'option sélectionnée en première position dans le conteneur '.dropdown-buttons'.
-  const dropdown = document.querySelector('.dropdown-buttons');
-  dropdown.insertBefore(selectedOption, dropdown.firstChild);
-}
-
-toggleOptions();
+// toggleOptions();
 initPhotographerPage();
-// Définit la fonction qui s'occupe de l'ajout des écouteurs d'événements sur les options.
-document.addEventListener('click', (event) => {
-  const dropdown = document.querySelector('.dropdown');
-  const isDropdownClick = dropdown.contains(event.target);
-  const options = document.querySelectorAll('.dropdown-buttons button');
-
-  if (!isDropdownClick) {
-    options.forEach((option, index) => {
-      if (index > 0) {
-        // ignorer la première option
-        option.classList.add('not-displayed-options');
-      }
-    });
-    toggleDropdownArrows();
-    isDropdownOpen = false;
-  }
-});
 
 function toggleDropdownArrows() {
   const arrowUp = document.querySelector('.fa-chevron-up');
   const arrowDown = document.querySelector('.fa-chevron-down');
 
   if (!isDropdownOpen) {
-    arrowUp.classList.add('displayed-arrow');
-    arrowUp.classList.remove('not-displayed-arrow');
-    arrowDown.classList.add('not-displayed-arrow');
-    arrowDown.classList.remove('displayed-arrow');
-  } else {
     arrowUp.classList.add('not-displayed-arrow');
     arrowUp.classList.remove('displayed-arrow');
     arrowDown.classList.add('displayed-arrow');
     arrowDown.classList.remove('not-displayed-arrow');
+  } else {
+    arrowUp.classList.add('displayed-arrow');
+    arrowUp.classList.remove('not-displayed-arrow');
+    arrowDown.classList.add('not-displayed-arrow');
+    arrowDown.classList.remove('displayed-arrow');
   }
 }
--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -1,50 +1,111 @@
+//opti
+// export function mediaTemplate(media, name) {
+//   const { title, likes, image, video } = media;
+
+//   let photographerFirstName = name.split(' ')[0];
+
+//   if (photographerFirstName.includes('-')) {
+//     photographerFirstName = photographerFirstName.replace('-', '');
+//   }
+
+//   function createMedia() {
+//     const mediaName = document.createElement('p');
+//     mediaName.textContent = title ?? 'titre inconnu';
+//     mediaName.classList.add('media-title');
+
+//     const mediaLikes = document.createElement('p');
+//     mediaLikes.textContent = likes ?? '';
+//     const mediaHeart = document.createElement('i');
+//     mediaHeart.classList.add('fa-solid');
+//     mediaHeart.classList.add('fa-heart');
+//     const heartAndNumber = document.createElement('div');
+//     heartAndNumber.classList.add('heart-and-number');
+//     heartAndNumber.appendChild(mediaHeart);
+//     heartAndNumber.appendChild(mediaLikes);
+
+//     const nameAndLikesContainer = document.createElement('div');
+//     nameAndLikesContainer.classList.add('name-likes-container');
+//     nameAndLikesContainer.appendChild(mediaName);
+//     nameAndLikesContainer.appendChild(heartAndNumber);
+
+//     const mediaContainer = document.createElement('div');
+//     mediaContainer.classList.add('media-container');
+
+//     if (video) {
+//       const mediaPath = `./FishEye_Photos/Sample_Photos/${photographerFirstName}/${video}`;
+//       const mediaVideo = document.createElement('video');
+//       mediaVideo.setAttribute('src', mediaPath);
+//       mediaVideo.setAttribute('alt', title);
+//       mediaVideo.setAttribute('controls', 'controls');
+//       mediaContainer.appendChild(mediaVideo);
+//     } else {
+//       const mediaPath = `./FishEye_Photos/Sample_Photos/${photographerFirstName}/${image}`;
+//       const mediaImage = document.createElement('img');
+//       mediaImage.setAttribute('src', mediaPath);
+//       mediaImage.setAttribute('alt', title);
+//       mediaContainer.appendChild(mediaImage);
+//     }
+
+//     mediaContainer.appendChild(nameAndLikesContainer);
+//     return mediaContainer;
+//   }
+//   return {
+//     title,
+//     likes,
+//     image,
+//     video,
+//     createMedia,
+//   };
+// }
 export function mediaTemplate(media, name) {
   const { title, likes, image, video } = media;
 
   let photographerFirstName = name.split(' ')[0];
-  // console.log('prénom:', photographerFirstName);
 
   if (photographerFirstName.includes('-')) {
-    // Si le prénom contient un tiret, supprimez-le
     photographerFirstName = photographerFirstName.replace('-', '');
   }
 
-  // Utilisez le prénom modifié
-  // console.log('prénom après modification:', photographerFirstName);
-
-  // console.log('titre:', title, 'nombre likes:', likes, 'image:', image);
-
-  // console.log(mediaPath);
-
   function createMedia() {
+    const mediaName = document.createElement('p');
+    mediaName.textContent = title ?? 'titre inconnu';
+    mediaName.classList.add('media-title');
+
+    const mediaLikes = document.createElement('p');
+    mediaLikes.textContent = likes ?? '';
+    const mediaHeart = document.createElement('i');
+    mediaHeart.classList.add('fa-solid');
+    mediaHeart.classList.add('fa-heart');
+    const heartAndNumber = document.createElement('div');
+    heartAndNumber.classList.add('heart-and-number');
+    heartAndNumber.appendChild(mediaLikes);
+    heartAndNumber.appendChild(mediaHeart);
+
+    const nameAndLikesContainer = document.createElement('div');
+    nameAndLikesContainer.classList.add('name-likes-container');
+    nameAndLikesContainer.appendChild(mediaName);
+    nameAndLikesContainer.appendChild(heartAndNumber);
+
+    const mediaContainer = document.createElement('div');
+    mediaContainer.classList.add('media-container');
+
     if (video) {
       const mediaPath = `./FishEye_Photos/Sample_Photos/${photographerFirstName}/${video}`;
       const mediaVideo = document.createElement('video');
       mediaVideo.setAttribute('src', mediaPath);
       mediaVideo.setAttribute('alt', title);
       mediaVideo.setAttribute('controls', 'controls');
-      const mediaName = document.createElement('p');
-      mediaName.textContent = title ?? 'titre inconnu';
-      mediaName.classList.add('media-title');
-      const mediaContainer = document.createElement('div');
-      mediaContainer.classList.add('media-container');
       mediaContainer.appendChild(mediaVideo);
-      mediaContainer.appendChild(mediaName);
-      return mediaContainer;
     } else {
       const mediaPath = `./FishEye_Photos/Sample_Photos/${photographerFirstName}/${image}`;
       const mediaImage = document.createElement('img');
       mediaImage.setAttribute('src', mediaPath);
       mediaImage.setAttribute('alt', title);
-      const mediaName = document.createElement('p');
-      mediaName.classList.add('media-title');
-      mediaName.textContent = title ?? 'titre inconnu';
-      const mediaContainer = document.createElement('div');
-      mediaContainer.classList.add('media-container');
       mediaContainer.appendChild(mediaImage);
-      mediaContainer.appendChild(mediaName);
-      return mediaContainer;
     }
+
+    mediaContainer.appendChild(nameAndLikesContainer);
+    return mediaContainer;
   }
   return {
     title,
