a1b34929d9057a20758ee24fede1b3320c4b1a54
Mehdi Charef <me-charef@hotmail.fr>
2024-03-14 23:12:02 +0100

    comments

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -120,12 +120,16 @@ async function createMedias(photographer) {
 }
 
 
+// Récupère les données d'un photographe spécifique par son ID
 async function getPhotographer() {
 	try {
+		// Appel asynchrone pour récupérer tous les photographes
 		const datas = await getPhotographers();
+		// Filtre pour trouver le photographe par ID
 		photographer = datas.photographers?.filter(
 			(photographer) => photographer.id === parseInt(id)
 		)[0];
+		// Filtre les médias associés au photographe par son ID
 		const photographerMedias = datas.media.filter(
 			(oneMedia) => oneMedia.photographerId == id
 		);
@@ -133,26 +137,30 @@ async function getPhotographer() {
 
 		return photographer;
 	} catch (error) {
+		// Gère les erreurs lors de la récupération des données
 		console.error('Erreur lors de la récupération des photographes:', error);
 	}
 }
 
+
+// Initialisation de la page du photographe
 async function initPhotographerPage() {
+	// Récupération du photographe par son ID
 	photographer = await getPhotographer();
 
+	// Calcul du total des likes pour les médias affichés
 	if (displayedMedias) {
 		for (let oneMedia of displayedMedias) {
 			totalLikes += oneMedia.likes;
 		}
 	}
 
+	// Si un photographe a été trouvé
 	if (photographer) {
-		const photographerModelSinglePage = photographerTemplate(
-			photographer,
-			true,
-			'h1',
-			totalLikes
-		);
+		// Création du modèle de la page du photographe
+		const photographerModelSinglePage = photographerTemplate(photographer, true, 'h1', totalLikes);
+			
+		// Si le photographe a un tarif défini, affichage du tarif et du total des likes
 		if (photographer?.price) {
 			const priceAndTotalLikes = document.createElement('div');
 			const price = document.createElement('p');
@@ -163,6 +171,7 @@ async function initPhotographerPage() {
 			mainContainer.appendChild(priceAndTotalLikes);
 		}
 
+		// Ajout du modèle du photographe au DOM
 		const photographerCardDom = photographerModelSinglePage.getUserCardDOM();
 		const photographerHeader = document.querySelector('.photograph-header');
 		photographerHeader.appendChild(photographerCardDom);
@@ -170,10 +179,12 @@ async function initPhotographerPage() {
 		console.log('Aucun photographe trouvé avec cet ID.');
 	}
 
+	// Création des médias associés au photographe
 	if (displayedMedias) {
 		createMedias(photographer);
 	}
 
+	// Génération des boutons du menu déroulant
 	generateDropdownButtons();
 }
 
@@ -195,15 +206,19 @@ function sortByTitle() {
 initPhotographerPage();
 
 function toggleDropdownArrows() {
+	// Sélection des éléments flèche dans le DOM
 	const arrowUp = document.querySelector('.fa-chevron-up');
 	const arrowDown = document.querySelector('.fa-chevron-down');
 
+	// Basculer la visibilité des flèches en fonction de l'état du menu déroulant
 	if (!isDropdownOpen) {
+		// Menu fermé : affiche flèche vers le bas, cache flèche vers le haut
 		arrowUp.classList.add('not-displayed-arrow');
 		arrowUp.classList.remove('displayed-arrow');
 		arrowDown.classList.add('displayed-arrow');
 		arrowDown.classList.remove('not-displayed-arrow');
 	} else {
+		// Menu ouvert : affiche flèche vers le haut, cache flèche vers le bas
 		arrowUp.classList.add('displayed-arrow');
 		arrowUp.classList.remove('not-displayed-arrow');
 		arrowDown.classList.add('not-displayed-arrow');
@@ -215,16 +230,6 @@ function closeModal() {
 	const contactModal = document.querySelector('#contact_modal');
 	contactModal.style.display = 'none';
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-	const sendButton = document.querySelector('.send_button');
-	if (sendButton) {
-		sendButton.addEventListener('click', (e) => {
-			e.preventDefault();
-			getFormValues();
-		});
-	}
-});
 
 function getFormValues() {
 	const firstName = document.getElementById('firstName').value;
@@ -238,87 +243,121 @@ function getFormValues() {
 		message: textarea,
 	};
 
-	closeModal();
 	console.log('form', formDetails);
+	closeModal();
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+	const sendButton = document.querySelector('.send_button');
+	if (sendButton) {
+		sendButton.addEventListener('click', (e) => {
+			e.preventDefault();
+			getFormValues();
+		});
+	}
+});
+
+// Gère les événements de clic sur les icônes de like
 function handleLikes(event) {
+	// Vérifie si l'élément cliqué est une icône "regular" (non liké)
 	if (event.target.classList.contains('fa-regular')) {
+		// Change l'icône à "solid" (liké), indiquant visuellement que l'élément a été liké
 		event.target.classList.remove('fa-regular');
 		event.target.classList.add('fa-solid');
+		// Met à jour le nombre de likes du média et le total global de likes
 		changeMediaNumberOfLikes(true, event.target);
 		updateTotalLikes();
 	} else {
+		// Si l'icône est déjà "solid" (liké), la change à "regular" (non liké)
 		event.target.classList.add('fa-regular');
 		event.target.classList.remove('fa-solid');
+		// Diminue le nombre de likes du média et met à jour le total global
 		changeMediaNumberOfLikes(false, event.target);
 		updateTotalLikes(false);
 	}
 }
 
+// Modifie le nombre de likes pour un média spécifique
 function changeMediaNumberOfLikes(isMore = true, target) {
+	// Sélectionne l'élément affichant le nombre de likes adjacent à l'icône
+	let heart = target;
+	let number = heart.previousElementSibling;
+	let numberOfLikes = parseInt(number.textContent);
+
+	// Augmente ou diminue le nombre de likes selon l'action de l'utilisateur
 	if (isMore) {
-		let heart = target;
-		let number = heart.previousElementSibling;
-		let numberOfLikes = parseInt(number.textContent);
-		numberOfLikes += 1;
-		number.textContent = numberOfLikes;
+		numberOfLikes += 1; // Incrémente pour un like
 	} else {
-		let heart = target;
-		let number = heart.previousElementSibling;
-		let numberOfLikes = parseInt(number.textContent);
-		numberOfLikes -= 1;
-		number.textContent = numberOfLikes;
+		numberOfLikes -= 1; // Décrémente pour un dislike
 	}
+	// Met à jour l'affichage du nombre de likes
+	number.textContent = numberOfLikes;
 }
 
+
+// Met à jour le total des likes affiché sur la page
 function updateTotalLikes(isMore = true) {
+	// Sélectionne l'élément HTML qui affiche le total des likes
 	let totalLikesElement = document.querySelector('.number-of-likes');
+	
+	// Vérifie si l'élément a été trouvé
 	if (totalLikesElement) {
-		if (isMore) {
-			totalLikes += 1;
-		} else {
-			totalLikes -= 1;
-		}
+		// Incrémente ou décrémente le total des likes basé sur le paramètre isMore
+		totalLikes += isMore ? 1 : -1;
+			
+		// Met à jour l'affichage avec le nouveau total des likes
 		totalLikesElement.textContent = totalLikes;
 	} else {
-		// Gestion de l'erreur ou tentative de récupération de l'élément à nouveau
+		// Affiche une erreur dans la console si l'élément n'est pas trouvé
 		console.error('L\'élément .number-of-likes n\'a pas été trouvé dans le DOM.');
 	}
 }
 
+// Ouvre la lightbox pour afficher les médias en grand format
 function openLightbox(mediaPath, imgTitle, index) {
+	// Met à jour l'index courant pour la navigation dans la lightbox
 	currentLightboxIndex = index;
+	
+	// Affiche la lightbox en rendant son conteneur visible
 	const lightbox = document.getElementById('lightbox_modal');
 	lightbox.style.display = 'block';
+	
+	// Cache le conteneur principal pour mettre en évidence la lightbox
 	const mainContainer = document.querySelector('.main-container');
 	mainContainer.style.display = 'none';
-	const lightboxMediaContainer = document.querySelector(
-		'.lightbox-media-container'
-	);
-	lightboxMediaContainer.innerHTML = '';
+	
+	// Prépare le conteneur interne de la lightbox pour le nouveau média
+	const lightboxMediaContainer = document.querySelector('.lightbox-media-container');
+	lightboxMediaContainer.innerHTML = ''; // Efface les médias précédents
+
+	// Vérifie le type de média (vidéo ou image) basé sur l'extension du fichier
 	if (mediaPath.includes('mp4')) {
+		// Crée un élément vidéo si le média est une vidéo
 		const lightboxVideo = document.createElement('video');
 		lightboxVideo.classList.add('lightbox-img');
 		lightboxVideo.setAttribute('src', mediaPath);
-		lightboxVideo.setAttribute('controls', 'controls');
+		lightboxVideo.setAttribute('controls', 'controls'); // Ajoute les contrôles de lecture
 		const lightboxImgTitle = document.createElement('p');
 		lightboxImgTitle.classList.add('lightboxImgTitle');
-		lightboxImgTitle.textContent = imgTitle;
+		lightboxImgTitle.textContent = imgTitle; // Définit le titre de la vidéo
 		lightboxMediaContainer.appendChild(lightboxVideo);
-		lightboxMediaContainer.appendChild(lightboxImgTitle);
+		lightboxMediaContainer.appendChild(lightboxImgTitle); // Ajoute vidéo et titre au conteneur
 	} else {
+		// Crée un élément image si le média est une image
 		const lightboxImg = document.createElement('img');
 		lightboxImg.classList.add('lightbox-img');
-		lightboxImg.setAttribute('src', mediaPath);
+		lightboxImg.setAttribute('src', mediaPath); // Définit le chemin de l'image
 		const lightboxImgTitle = document.createElement('p');
 		lightboxImgTitle.classList.add('lightboxImgTitle');
-		lightboxImgTitle.textContent = imgTitle;
-		lightboxMediaContainer.appendChild(lightboxImg);
+		lightboxImgTitle.textContent = imgTitle; // Définit le titre de l'image
+		lightboxMediaContainer.appendChild(lightboxImg); // Ajoute image et titre au conteneur
 		lightboxMediaContainer.appendChild(lightboxImgTitle);
 	}
+
 	handlePrevButton();
 	handleNextButton();
+
+	// Gère les événements du clavier pour la navigation et la fermeture
 	handleKeyboardEvents();
 }
 
@@ -336,43 +375,51 @@ document
 	.querySelector('.close-lightbox')
 	.addEventListener('click', closeLightbox);
 
+// Gère l'action de navigation vers le média précédent dans la lightbox
 function handlePrevButton() {
-	console.log(displayedMedias);
-	const lightboxMediaContainer = document.querySelector(
-		'.lightbox-media-container'
-	);
 
+	// Sélectionne le conteneur de la lightbox pour afficher le média
+	const lightboxMediaContainer = document.querySelector('.lightbox-media-container');
+
+	// Récupère le chemin de l'image ou de la vidéo actuellement affichée
 	const currentImageSrc = document.querySelector('.lightbox-img').src;
+	// Divise le chemin en segments pour pouvoir remplacer le nom du fichier
 	const imgSourceArray = currentImageSrc.split('/');
+	// Efface le contenu précédent du conteneur pour le nouveau média
 	lightboxMediaContainer.innerHTML = '';
 
-	let newImageIndex;
+	let newImageIndex; // Déclare une variable pour le nouvel index du média
+
+	// Calcule le nouvel index, gère le cas où l'index actuel est le premier élément
 	if (currentLightboxIndex - 1 < 0) {
-		newImageIndex = displayedMedias.length - 1;
-		currentLightboxIndex = newImageIndex;
+		newImageIndex = displayedMedias.length - 1; // Revient au dernier média si on est au début
 	} else {
-		newImageIndex = currentLightboxIndex - 1;
-		currentLightboxIndex = newImageIndex;
+		newImageIndex = currentLightboxIndex - 1; // Sinon, décrémente simplement l'index
 	}
-	const newImageName =
-    displayedMedias[newImageIndex].image ||
-    displayedMedias[newImageIndex].video;
+	currentLightboxIndex = newImageIndex; // Met à jour l'index courant
 
-	imgSourceArray[imgSourceArray.length - 1] = newImageName;
-	let newSrc = imgSourceArray.join('/');
+	// Détermine le nouveau chemin du média en fonction de son type (image ou vidéo)
+	const newImageName = displayedMedias[newImageIndex].image || displayedMedias[newImageIndex].video;
+	imgSourceArray[imgSourceArray.length - 1] = newImageName; // Remplace le nom du fichier dans le chemin
+	let newSrc = imgSourceArray.join('/'); // Recombine les segments en un chemin complet
 
+	// Crée un nouvel élément HTML (img ou video) selon le type de média
 	const newLightboxImg = displayedMedias[newImageIndex].hasOwnProperty('video')
 		? document.createElement('video')
 		: document.createElement('img');
-	newLightboxImg.setAttribute('src', newSrc);
+	newLightboxImg.setAttribute('src', newSrc); // Définit le chemin du nouveau média
 
-	if (newSrc.includes('mp4')) {
+	// Si c'est une vidéo, ajoute les contrôles de lecture
+	if (displayedMedias[newImageIndex].hasOwnProperty('video')) {
 		newLightboxImg.setAttribute('controls', '');
 	}
 
+	
+	// Ajoute le nouvel élément média au conteneur
 	newLightboxImg.classList.add('lightbox-img');
 	lightboxMediaContainer.appendChild(newLightboxImg);
 
+	// Crée et ajoute le titre du média au conteneur
 	const lightboxImgTitle = document.createElement('p');
 	lightboxImgTitle.classList.add('lightboxImgTitle');
 	lightboxImgTitle.textContent = displayedMedias[newImageIndex].title;
@@ -380,46 +427,48 @@ function handlePrevButton() {
 }
 
 function handleNextButton() {
-	const lightboxMediaContainer = document.querySelector(
-		'.lightbox-media-container'
-	);
+	// Sélectionne le conteneur de la lightbox pour le nouveau média
+	const lightboxMediaContainer = document.querySelector('.lightbox-media-container');
 
+	// Extrai le chemin actuel du média affiché pour déterminer le prochain média
 	const currentImageSrc = document.querySelector('.lightbox-img').src;
 	const imgSourceArray = currentImageSrc.split('/');
-	lightboxMediaContainer.innerHTML = '';
+	lightboxMediaContainer.innerHTML = ''; // Prépare le conteneur pour le nouveau contenu
 
+	// Détermine le nouvel index pour afficher le média suivant
 	let newImageIndex;
 	if (currentLightboxIndex + 1 > displayedMedias.length - 1) {
-		newImageIndex = 0;
-		currentLightboxIndex = newImageIndex;
+		newImageIndex = 0; // Retourne au début si on dépasse le dernier média
 	} else {
-		newImageIndex = currentLightboxIndex + 1;
-		currentLightboxIndex = newImageIndex;
+		newImageIndex = currentLightboxIndex + 1; // Sinon, passe au média suivant
 	}
-	const newImageName =
-    displayedMedias[newImageIndex].image ||
-    displayedMedias[newImageIndex].video;
+	currentLightboxIndex = newImageIndex;
 
+	// Construit le chemin du nouveau média en remplaçant le nom de fichier
+	const newImageName = displayedMedias[newImageIndex].image || displayedMedias[newImageIndex].video;
 	imgSourceArray[imgSourceArray.length - 1] = newImageName;
 	let newSrc = imgSourceArray.join('/');
 
-	const newLightboxImg = newSrc.includes('mp4')
+	// Crée l'élément HTML approprié pour le type de média (image ou vidéo)
+	const newLightboxImg = displayedMedias[newImageIndex].hasOwnProperty('video')
 		? document.createElement('video')
 		: document.createElement('img');
 	newLightboxImg.setAttribute('src', newSrc);
 
-	if (newSrc.includes('mp4')) {
+	// Ajoute les contrôles de lecture pour les vidéos
+	if (displayedMedias[newImageIndex].hasOwnProperty('video')) {
 		newLightboxImg.setAttribute('controls', '');
 	}
 
+	// Ajoute le nouvel élément média et son titre au conteneur de la lightbox
 	newLightboxImg.classList.add('lightbox-img');
 	lightboxMediaContainer.appendChild(newLightboxImg);
-
 	const lightboxImgTitle = document.createElement('p');
 	lightboxImgTitle.classList.add('lightboxImgTitle');
 	lightboxImgTitle.textContent = displayedMedias[newImageIndex].title;
 	lightboxMediaContainer.appendChild(lightboxImgTitle);
 }
+
 
 function handleKeyboardEvents() {
 	document.addEventListener('keydown', (event) => {
@@ -447,14 +496,3 @@ document
 	.querySelector('#nextButton')
 	.addEventListener('click', handleNextButton);
 
-//meme chose fleche gauche puis refacto dans 2 fonctions à appeler au click et au keydown
-
-// document.addEventListener('keydown', (event) => {
-//   if (event.key === 'ArrowRight') {
-//     document.querySelector('#nextButton').click();
-//   } else if (event.key === 'ArrowLeft') {
-//     document.querySelector('#prevButton').click();
-//   } else if (event.key === 'Escape') {
-//     closeLightbox();
-//   }
-// });
