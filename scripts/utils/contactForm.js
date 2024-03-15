775f2be7460f66215309abcee83c7ee0bef0130c
Mehdi Charef <me-charef@hotmail.fr>
2024-03-15 12:14:28 +0100

    contact modal tab + aria labels

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -2,10 +2,20 @@
 function displayModal() {
 	const modal = document.getElementById('contact_modal');
 	modal.style.display = 'block';
-}
+	
+	// Met le focus sur le premier élément focusable de la modale
+	modal.querySelector('button, input, textarea').focus();
 
+	// Ajoute un écouteur d'événements pour fermer la modale avec la touche Échappement
+	document.addEventListener('keydown', function(event) {
+		if (event.key === 'Escape') {
+			closeModal();
+		}
+	});
+}
 // eslint-disable-next-line no-unused-vars
 function closeModal() {
 	const modal = document.getElementById('contact_modal');
 	modal.style.display = 'none';
 }
+
